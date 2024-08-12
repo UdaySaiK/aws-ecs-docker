296e22ece6a52f2946c4673f08dfa561fe085dab
UdaySaiK <138034987+UdaySaiK@users.noreply.github.com>
2024-08-12 09:39:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my app qq !!!!!!");
+  res.send("Welcome to my app qqq !!!!!!");
 });
 
 app.get("/info", (req, res) => {
